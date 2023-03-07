4b64f096e8080cac34ef49f50983fc926d76782b
Joshua Dupin <joshua35260@gmail.com>
2023-03-07 09:54:29 +0100

    done

---
+++ src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
@@ -0,0 +1,26 @@
+package com.wildcodeschool.myProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+  @GetMapping("/doctor/1")
+  @ResponseBody
+    public String doctor1() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/2")
+    @ResponseBody
+    public String doctor2() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/3")
+    @ResponseBody
+    public String doctor3() {
+        return "Jodie Whittaker";
+    }
+}
